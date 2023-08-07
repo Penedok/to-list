3af5d3b3afef3f518ccce036437ada3deb532a39
Karen Penedo <penedok12@gmail.com>
2023-08-06 21:30:22 -0400

    colhendo os dados do input tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,43 @@
-import 
+import React from "react";
+import { useState } from "react";
 
 function App() {
- 
+
+  const [tarefas, setTarefas] = useState('');
+
+  function handleEnviaTarefa(e){
+    e.preventDefault();
+      console.log(tarefas)
+  }
+  
+
 
   return (
     <>
-      <h2>Olá</h2>
+      <section>
+        <div>
+          <h2>To List</h2>
+          <p>Selecione quem é você!</p>
+        </div>
+        <div>
+          <select>
+            <option>Organizador</option>
+            <option>Executador</option>
+          </select>
+        </div>
+        <div>
+          <form onSubmit={handleEnviaTarefa}>
+            <label>Nome da Tarefa</label>
+            <input  type="text"  name="tarefa" onChange={(e) =>{
+               setTarefas({tarefas:e.target.value}) 
+            }}/>
+            <input type="submit"/>
+             
+          </form>
+        </div>
+      </section>  
+     
+      
     </>
   )
 }
