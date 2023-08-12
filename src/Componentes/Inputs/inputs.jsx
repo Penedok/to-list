d2d2cea03470ce16eccb6175aa80a1d1b1ba3ef5
Karen Penedo <penedok12@gmail.com>
2023-08-12 17:26:16 -0400

    estilizando o cartão do formulario e a section

--- src/Componentes/Inputs/inputs.jsx
+++ src/Componentes/Inputs/inputs.jsx
@@ -1,30 +1,33 @@
 
 
 
+import {ContainerInput, Selecao, Dados,Cartao,Select} from './styleInputs'
+
 export default function Inputs (){
     return(
-        <div>
-           <div>
-              <h2>Diga-nos quem é você?!</h2>
-              <select>
-                <option>Selecione..</option>
-                <option>Organizador</option>
-                <option>Executador</option>
-              </select>
-           </div>
-           <div>
-              <form>
-                 <div>
-                    <label>Nome</label>
-                    <input type="text" name="Tarefa"/>
-                 </div>
-                 <div>
-                    <button type="submit">Enviar</button>
-                 </div>
-              </form>
-           </div>
-
-        </div>
-      
+        <ContainerInput>
+           <Cartao>
+                <Selecao>
+                   <h2>Diga-nos quem é você...</h2>
+                   <Select>
+                     <option>Selecione..</option>
+                     <option>Organizador</option>
+                     <option>Executador</option>
+                   </Select>
+                </Selecao>
+                <Dados>
+                    <form>
+                        <div>
+                           <label>Nome</label>
+                           <input type="text" name="Tarefa"/>
+                         </div>
+                         <div>
+                           <button type="submit">Enviar</button>
+                         </div>
+                    </form>
+                </Dados>
+            </Cartao>  
+        </ContainerInput>
+        
     )
 }
---
+++ src/Componentes/Inputs/styleInputs.jsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+
+export const ContainerInput = styled.div`
+    display:flex;
+    justify-content: center;
+    flex-direction:column;
+    aling-items:center;
+`
+export const Cartao = styled.div`
+    display:flex;
+    justify-content: center;
+    flex-direction:column;
+    aling-items:center;
+    background: linear-gradient(180deg, rgba(26,36,0,1) 0%, rgba(95,177,61,1) 3%, rgba(0,212,255,1) 100%) no-repeat;
+    margin: 2.5em 6em ;
+    border:1px solid transparent;
+    border-radius:1.5em;
+
+`
+export const Selecao =styled.div`
+    display:flex;
+    justify-content: center;
+    
+    aling-items:center;
+`
+export const Dados = styled.div`
+    display:flex;
+    justify-content: center;
+    aling-items:center;
+`
+export const Select = styled.select`
+  margin:1.7em 0.8em;
+  border: 1px solid transparent;
+  border-radius:2em;
+  padding: 0 1em;  
+`
